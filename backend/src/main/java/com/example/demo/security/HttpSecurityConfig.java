a6a7d56196d1b25aa59adcf3dd4101c613d860c8
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-05-29 19:19:16 -0300

    Add security config

---
+++ backend/src/main/java/com/example/demo/security/HttpSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class HttpSecurityConfig {
+
+    @Autowired
+    private AuthenticationProvider daoAuthenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        SecurityFilterChain filterChain = http
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionManagementConfig -> sessionManagementConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(daoAuthenticationProvider)
+                .authorizeHttpRequests(authRequest -> authRequest
+                        .anyRequest().permitAll())
+                .build();
+
+        return filterChain;
+    }
+}
